057d423a2bfbd5330b469f3f67ac2ccc71c8b896
bin.luo <bin.luo@CNbin.luo>
2019-07-29 18:13:37 +0800

    feat: add java files

---
+++ java/ArrayHomework.java
@@ -0,0 +1,13 @@
+// import java.util.Arrays;
+
+public class ArrayHomework {
+
+    public static void main(String[] args) {
+        int[] array = { 6, 17, 92, 32, 58, 22, 84, 66, 36, 33 };
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 33) {
+                System.out.println(i);
+            }
+        }
+    }
+}
